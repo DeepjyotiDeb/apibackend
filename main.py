830c3dd5bd29d2175dc3d0b12f856b41e2940d12
DeepjyotiDeb <55659244+DeepjyotiDeb@users.noreply.github.com>
2022-01-11 19:03:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -34,10 +34,7 @@ app.add_middleware(
 @app.get('/', tags = ['blogs']) # all posts
 def sanity(db:Session = Depends(get_db)):
     posts = db.query(models.BlogDetails).all()
-    return {
-        "posts": posts,
-        # "origins":origins
-    }
+    return (posts, "hello boi")
 
 @app.get('/posts', tags = ['blogs']) # all posts
 def show_posts(db:Session = Depends(get_db)):
