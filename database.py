bf852c30107332b99c7626f8b49deab68a13df14
DeepjyotiDeb <55659244+DeepjyotiDeb@users.noreply.github.com>
2022-01-28 00:42:25 +0530

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 import urllib
+import os 
 
+DATABASE_URL = os.environ.get('DATABASE_URL')
 # host_server = os.environ.get('host_server', 'localhost')
 # db_server_port = urllib.parse.quote_plus(str(os.environ.get('db_server_port', '5432')))
 # database_name = os.environ.get('database_name', 'fastapi')
@@ -12,7 +14,7 @@ import urllib
 # ssl_mode = urllib.parse.quote_plus(str(os.environ.get('ssl_mode','prefer')))
 # DATABASE_URL = 'postgresql://{}:{}@{}:{}/{}?sslmode={}'.format(db_username, db_password, host_server, db_server_port, database_name, ssl_mode)
 
-DATABASE_URL = 'postgresql://bcousmwcngxnrr:27b77cee0eded7509d078d6c5cfef835beea60c66e5140d2991e043cd0beb010@ec2-63-32-30-191.eu-west-1.compute.amazonaws.com:5432/d970v91be4husa'
+# DATABASE_URL = 'postgresql://bcousmwcngxnrr:27b77cee0eded7509d078d6c5cfef835beea60c66e5140d2991e043cd0beb010@ec2-63-32-30-191.eu-west-1.compute.amazonaws.com:5432/d970v91be4husa'
 
 engine = create_engine(DATABASE_URL)
 
